70db20a79e8d7ff94f24a0fcbb0fffaf7f3a2538
MarcosLeva <marcoslevagomez@gmail.com>
2024-02-02 23:55:31 -0700

    effect in input

--- src/components/common/InputReturn.tsx
+++ src/components/common/InputReturn.tsx
@@ -2,7 +2,8 @@
 import React, { ChangeEvent, useEffect, useState } from "react";
 import Image from "next/image";
 import useUserRegistrationStore from "@/store/userRegistrationStore";
-
+import { TextField } from "@mui/material";
+import { classNamesCustom } from "@/utils/classes";
 interface Props {
   icon: string;
   placeholder: string;
@@ -20,19 +21,32 @@ const InputReturn = ({
   onChange,
 }: Props) => {
   return (
-    <div className="relative  w-full h-[51px]">
-      <span className="absolute inset-y-6 left-0 flex items-center pl-[16px]">
-        <Image src={`${icon}.svg`} width={16} height={16} alt="" />
-      </span>
-      <input
-        className="pl-10 border rounded-lg focus:outline-none  text-white bg-inputs border-none w-full h-full flex items-center px-[12px] placeholder:text-[12px] placeholder:text-white placeholder:opacity-50 placeholder:font-[600] placeholder:leading-[120%"
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        type={type || "text"}
-        name={name}
-      />
-    </div>
+    <>
+      <div className="relative  w-full max-h-[51px]  min-h-[51px]">
+        <span className="absolute inset-y-6 left-0 flex items-center pl-[16px]">
+          <Image src={`${icon}.svg`} width={16} height={16} alt="" />
+        </span>
+        <div className="pl-10 flex flex-col bg-inputs rounded-[8px] py-[8px] max-h-[51px] min-h-[51px] justify-center">
+          {value && (
+            <label className="text-[10px] font-[700] opacity-50 text-left">
+              {placeholder}
+            </label>
+          )}
+          <input
+            className={classNamesCustom(
+              "border rounded-lg focus:outline-none bg-inputs max-h-[51px] text-white border-none w-full h-full flex items-center  placeholder:text-white placeholder:opacity-50 placeholder:font-[600] placeholder:leading-[100%]",
+              { "md:text-[16px]": value },
+              { "md:text-[12px]": !value }
+            )}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            type={type || "text"}
+            name={name}
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
