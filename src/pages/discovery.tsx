4a8cf19cf680e5860aae42e42d4a7fd7257e0d53
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2024-02-12 21:58:04 -0600

    only render

--- src/pages/discovery.tsx
+++ src/pages/discovery.tsx
@@ -2,20 +2,28 @@ import React from "react";
 import DesktopLayout from "@/components/common/DesktopLayout";
 import VerticalMobileVideos from "@/components/discovery/VerticalMobileVideos";
 import VerticalDesktopVideos from "@/components/discovery/VerticalDesktopVideos";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 const ImageViewer = () => {
+  const { width } = useWindowSize();
+  const isMobile = width ?? 0 < 768;
+
   return (
     <DesktopLayout>
       <div
         id="discovery-container"
         className="fixed md:relative w-full max-h-screen md:overflow-y-auto"
       >
-        <div className="md:hidden">
-          <VerticalMobileVideos />
-        </div>
-        <div className="hidden md:block">
-          <VerticalDesktopVideos />
-        </div>
+        {isMobile && (
+          <div className="md:hidden">
+            <VerticalMobileVideos />
+          </div>
+        )}
+        {!isMobile && (
+          <div className="hidden md:block">
+            <VerticalDesktopVideos />
+          </div>
+        )}
       </div>
     </DesktopLayout>
   );
