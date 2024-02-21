39808ef436e10742dc30d987fb2a70a4b09b4d51
MarcosLeva <marcoslevagomez@gmail.com>
2024-02-20 22:07:29 -0700

    cambios de videos y estilos

--- src/components/dashboard/components/Sections/VideoCaroussel.tsx
+++ src/components/dashboard/components/Sections/VideoCaroussel.tsx
@@ -2,7 +2,8 @@
 import React, { Fragment } from "react";
 import VideoControllerDashboard from "@/components/discovery/VideoControllerDashboard";
 import Video from "@/components/dashboard/Video";
-
+import useIsMobile from "@/hooks/useIsMobile";
+import { classNamesCustom } from "@/utils/classes";
 interface Props {
   videos: string[];
   title: string;
@@ -10,12 +11,19 @@ interface Props {
 }
 
 const VideoCaroussel = ({ videos, title, id }: Props) => {
+  const isMobile = useIsMobile();
   return (
-    <div className="flex flex-col gap-y-[8px]">
+    <div className="flex flex-col gap-y-[8px] relative">
       <div className="flex justify-between items-center px-[16px] z-50">
         <label className="text-[16px] font-[600] leading-[150%]">{title}</label>
       </div>
-      <div className="flex gap-x-[16px] items-center px-[16px] overflow-x-auto ">
+      <div
+        className={classNamesCustom(
+          "flex gap-x-[16px] items-center px-[16px] overflow-x-auto",
+          { "scrollnice pb-[8px]": !isMobile },
+          { hidescroll: isMobile }
+        )}
+      >
         {videos.map((video, i) => (
           <Fragment key={i}>
             <VideoControllerDashboard
@@ -28,6 +36,7 @@ const VideoCaroussel = ({ videos, title, id }: Props) => {
           </Fragment>
         ))}
       </div>
+      <div className="absolute bottom-0 left-0 w-full h-full pointer-events-none bg-bg-gradient-discovery-left hidden md:block"></div>
     </div>
   );
 };
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -13,6 +13,19 @@ const lo_que_nadie_te_dice = [
   "https://pub-bf9da7896edf4ee98e6d6dd8e72340c7.r2.dev/videos%2Flo_que_nadie_te_dice_de%2FJavier%20Ibarreche.mov",
 ];
 
+const tres_series = [
+  "https://pub-bf9da7896edf4ee98e6d6dd8e72340c7.r2.dev/videos%2F3%20series%2F3%20Series%20que%20no%20habias%20visto.mp4",
+  "https://pub-bf9da7896edf4ee98e6d6dd8e72340c7.r2.dev/videos%2F3%20series%2F3%20Series%20que%20no%20sabias%20que%20existian.mp4",
+  "https://pub-bf9da7896edf4ee98e6d6dd8e72340c7.r2.dev/videos%2F3%20series%2FSabias%20que%20de%20Griselda.mp4",
+];
+
+const resenias = [
+  "https://pub-bf9da7896edf4ee98e6d6dd8e72340c7.r2.dev/videos%2Frese%C3%B1as%2Fssstik.io_1708378327086.mp4",
+  "https://pub-bf9da7896edf4ee98e6d6dd8e72340c7.r2.dev/videos%2Frese%C3%B1as%2F7aea3551-69c3-4a48-a7ba-9a5c2e0eac44.MP4",
+  "https://pub-bf9da7896edf4ee98e6d6dd8e72340c7.r2.dev/videos%2Frese%C3%B1as%2F9a3b3dda-e4a0-45cd-83c2-5a06abb49b41.MP4",
+  "https://pub-bf9da7896edf4ee98e6d6dd8e72340c7.r2.dev/videos%2Frese%C3%B1as%2Fedc5d3c6-3038-4ea4-8f2c-59dfc5e6c128.MP4",
+];
+
 export default function Dashboard() {
   return (
     <DesktopLayout forceDisplay>
@@ -22,22 +35,8 @@ export default function Dashboard() {
           title="Lo que nadie te dice de..."
           id="1"
         />
-        <VideoCaroussel
-          videos={lo_que_nadie_te_dice}
-          title="Lo que nadie te dice de..."
-          id="2"
-        />
-        <VideoCaroussel
-          videos={lo_que_nadie_te_dice}
-          title="Lo que nadie te dice de..."
-          id="3"
-        />
-        <VideoCaroussel
-          videos={lo_que_nadie_te_dice}
-          title="Lo que nadie te dice de..."
-          id="4"
-        />
-        <div className="absolute bottom-0 left-0 w-full h-full pointer-events-none bg-bg-gradient-discovery-left hidden md:block"></div>
+        <VideoCaroussel videos={tres_series} title="Tres series" id="2" />
+        <VideoCaroussel videos={resenias} title="Reseñas" id="3" />
       </div>
       <Footer />
     </DesktopLayout>
