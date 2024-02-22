7633c39dd4dd8c6931b4489b28b011ae54757a1d
MarcosLeva <marcoslevagomez@gmail.com>
2024-02-21 20:49:35 -0700

    fix loading search

--- src/components/search/Sections.tsx
+++ src/components/search/Sections.tsx
@@ -19,6 +19,7 @@ const Sections = ({ text, endpoint }: Props) => {
 
   const getMovies = async () => {
     try {
+      setLoading(true);
       const movies_result = await api.get(`${endpoint}?q=${filters}`);
       setMovies(movies_result.data);
       setLoading(false);
@@ -33,53 +34,53 @@ const Sections = ({ text, endpoint }: Props) => {
 
   return (
     <>
-      {movies.length > 0 ? (
-        <div className="grid auto-flow-dense grid-layout w-auto">
-          <MostComponent text={text} />
-          <div
-            className={classNamesCustom(
-              ``,
-              {
-                "grid grid-cols-2 gap-x-[32px] gap-y-[16px] px-[16px] py-[12px] hidescroll w-fit mx-auto":
-                  isMobile,
-              },
-              {
-                "grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-x-[32px] gap-y-[16px] hidescroll w-fit mx-auto py-[16px] max-w-[1440px] overflow-x-hidden":
-                  !isMobile,
-              }
-            )}
-          >
-            {loading &&
-              Array(15)
-                .fill(null)
-                .map((_: any, index: number) => (
-                  <div
-                    key={index}
-                    className={classNames(
-                      "flex flex-col gap-y-[8px] max-w-[146px] min-w-[146px] md:max-w-[166px] md:min-w-[166px] animate-pulse",
-                      {
-                        "hidden md:flex": index > 3,
-                      }
-                    )}
-                  >
-                    <div className="bg-gray-300 rounded-[8px] w-[146px] h-[223px] md:w-[166px] md:h-[250px]"></div>
-                    <div className="flex flex-col gap-y-[4px]">
-                      <div className="bg-gray-300 rounded h-[20px] w-[100px]"></div>
-                      <div className="bg-gray-300 rounded h-[16px] w-[80px]"></div>
-                    </div>
+      <div className="grid auto-flow-dense grid-layout w-auto">
+        <MostComponent text={text} />
+        <div
+          className={classNamesCustom(
+            ``,
+            {
+              "grid grid-cols-2 gap-x-[32px] gap-y-[16px] px-[16px] py-[12px] hidescroll w-fit mx-auto":
+                isMobile,
+            },
+            {
+              "grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-x-[32px] gap-y-[16px] hidescroll w-fit mx-auto py-[16px] max-w-[1440px] overflow-x-hidden":
+                !isMobile,
+            }
+          )}
+        >
+          {loading &&
+            Array(15)
+              .fill(null)
+              .map((_: any, index: number) => (
+                <div
+                  key={index}
+                  className={classNames(
+                    "flex flex-col gap-y-[8px] max-w-[146px] min-w-[146px] md:max-w-[166px] md:min-w-[166px] animate-pulse",
+                    {
+                      "hidden md:flex": index > 3,
+                    }
+                  )}
+                >
+                  <div className="bg-gray-300 rounded-[8px] w-[146px] h-[223px] md:w-[166px] md:h-[250px]"></div>
+                  <div className="flex flex-col gap-y-[4px]">
+                    <div className="bg-gray-300 rounded h-[20px] w-[100px]"></div>
+                    <div className="bg-gray-300 rounded h-[16px] w-[80px]"></div>
                   </div>
-                ))}
+                </div>
+              ))}
 
-            {movies.map((item: any, index: number) => {
-              return <ItemSections key={index} props={item} />;
-            })}
-          </div>
+          {movies.map((item: any, index: number) => {
+            return <ItemSections key={index} props={item} />;
+          })}
+
+          {movies.length === 0 && !loading && (
+            <div className="flex justify-center items-center w-full">
+              No hay películas para mostrar.
+            </div>
+          )}
         </div>
-      ) : (
-        <div className="flex justify-center items-center">
-          No hay películas para mostrar.
-        </div>
-      )}
+      </div>
     </>
   );
 };
