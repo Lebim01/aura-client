a59ee0b90f7a08dd976d03105120a8e0da1b8306
MarcosLeva <marcoslevagomez@gmail.com>
2024-02-21 20:37:22 -0700

    new images

--- src/components/dashboard/components/filters/components/Platforms.tsx
+++ src/components/dashboard/components/filters/components/Platforms.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 const Platforms = ({ icon, value }: Props) => {
   return (
-    <div className="w-[78px] h-[78px] bg-black-18 flex justify-center rounded-[6px]">
+    <div className="w-full h-[78px] bg-black-18 flex justify-center rounded-[6px]">
       <Image src={icon} width={68} height={68} alt="" />
     </div>
   );
