e3b5590735a6473dedec36bfa81936814a49e86f
victor alvarez <victoralvarezsaucedo@gmail.com>
2024-02-25 15:13:40 -0600

    cursor

--- src/components/profile/Header.tsx
+++ src/components/profile/Header.tsx
@@ -9,17 +9,16 @@ const Header = ({ text }: Props) => {
   const router = useRouter();
 
   return (
-    <div
-      className="flex w-full p-[16px] items-center justify-between"
-      onClick={() => {
-        router.back();
-      }}
-    >
+    <div className="flex w-full p-[16px] items-center justify-between">
       <Image
         src={"/icons/flat-arrow-left-active.svg"}
         width={16}
         height={16}
         alt=""
+        className="hover:cursor-pointer"
+        onClick={() => {
+          router.back();
+        }}
       />
       <span className="text-[16px] font-[700] leading-[20.8px]">{text}</span>
       <div></div>
--- src/components/profile/Options.tsx
+++ src/components/profile/Options.tsx
@@ -1,12 +1,14 @@
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
 const Options = () => {
+  const { data } = useSession()
   const router = useRouter();
 
   return (
     <div className="flex flex-col w-full px-[16px]">
-      <div className="flex items-center justify-between w-full border-b border-b-border-otp py-[16px]">
+      {/*<div className="flex items-center justify-between w-full border-b border-b-border-otp py-[16px]">
         <div className="flex items-center gap-x-[8px]">
           <Image src={"/icons/user.svg"} alt="" width={20} height={20} />
           <span className="text-[14px]">@Jorge93</span>
@@ -17,8 +19,8 @@ const Options = () => {
           width={20}
           height={20}
         />
-      </div>
-      <div className="flex items-center justify-between w-full border-b border-b-border-otp py-[16px]">
+      </div>*/}
+      <div className="flex items-center justify-between w-full border-b border-b-border-otp py-[16px] hover:cursor-pointer">
         <div className="flex items-center gap-x-[8px]">
           <Image src={"/login/icons/lock.svg"} alt="" width={20} height={20} />
           <span className="text-[14px]">Actualizar contraseña</span>
@@ -31,7 +33,7 @@ const Options = () => {
         />
       </div>
       <div
-        className="flex items-center justify-between w-full border-b border-b-border-otp py-[16px]"
+        className="flex items-center justify-between w-full border-b border-b-border-otp py-[16px] hover:cursor-pointer"
         onClick={() => {
           router.push("/profile?step=general");
         }}
