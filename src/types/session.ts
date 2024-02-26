b0e58462856333addc69d174fb7bda307ec916ab
victor alvarez <victoralvarezsaucedo@gmail.com>
2024-02-26 14:22:58 -0600

    fix import

--- src/types/session.ts
+++ src/types/session.ts
@@ -1,5 +1,5 @@
 import { Profile } from "@/services/user";
-import { Session } from "next-auth";
+import type { Session } from "next-auth";
 
 export interface CustomSession extends Session {
     user: Profile;
