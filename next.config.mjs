269e1705d771f9cc9ea4da52f6a42c6c7d70538c
victor alvarez <victoralvarezsaucedo@gmail.com>
2024-02-26 18:37:01 -0600

    remote paths

--- next.config.mjs
+++ next.config.mjs
@@ -12,11 +12,10 @@ const nextConfig = {
     ];
   },
   images: {
-    domains: ["image.tmdb.org"],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "image.tmdb.org",
+        hostname: "**",
       },
     ],
   },
