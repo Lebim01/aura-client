dba56708ab76de35354f82513361da5f8b15ff8a
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2024-03-01 09:33:25 -0600

    no protect

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -5,7 +5,7 @@ import Sections from "@/components/search/Sections";
 
 const SearchPage = () => {
   return (
-    <AuthProvider protected>
+    <AuthProvider>
       <DesktopLayout forceDisplay>
         <div className="flex flex-col gap-y-[24px] overflow-y-auto w-auto pb-[99px] md:py-[32px] relative min-w-max flex-grow hidescroll md:items-start md:justify-start">
           <Sections text="Tu búsqueda" endpoint="/search/series" />
