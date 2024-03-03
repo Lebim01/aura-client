234c5f53a5d6602066ba3014e631e08d07a04587
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2024-03-03 15:07:41 -0600

    hide settings

--- src/components/profile/General.tsx
+++ src/components/profile/General.tsx
@@ -13,13 +13,13 @@ const optionAsistence = [{ label: "Privacidad y términos", url: "" }];
 const General = () => {
   return (
     <div className="flex flex-col px-[16px]">
-      <span className="pt-[16px] py-[24px] text-[16px] font-[700] leading-[150%] text-yellow-aura-accent">
+      {/*<span className="pt-[16px] py-[24px] text-[16px] font-[700] leading-[150%] text-yellow-aura-accent">
         General
       </span>
 
       {options.map((item: any, index: number) => {
         return <GeneralOption key={index} label={item.label} url={item.url} />;
-      })}
+      })}*/}
 
       <span className="pt-[16px] py-[24px] text-[16px] font-[700] leading-[150%] text-yellow-aura-accent">
         Asistencia
