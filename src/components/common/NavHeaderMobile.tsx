bffea8570d19c05214708ad6c8918284d122882d
victor alvarez <victoralvarezsaucedo@gmail.com>
2024-03-10 21:59:47 -0600

    pruebas

--- src/components/common/NavHeaderMobile.tsx
+++ src/components/common/NavHeaderMobile.tsx
@@ -10,26 +10,7 @@ const NavHeaderMobile = () => {
   
   return (
     <div className="pb-[16px] flex justify-between items-center">
-      <div className="font-bold">
-        {isLogged ? `Bienvenido ${data?.user.name}` : "Inicia Sesión"}
-      </div>
-      <div className="flex space-x-[8px]">
-        {/*<CircleButton
-          className="bg-[#0E0E0E] overflow-hidden"
-          style={{
-            borderImageSource:
-              "linear-gradient(143.13deg, #B8DE63 14.64%, #53D174 86.07%)",
-            boxShadow: "0px 0px 6px 0px #B6DE6480",
-          }}
-        >
-          <LuFilter size={20} />
-        </CircleButton>*/}
-        <Link href="/profile">
-          <CircleButton className="border-yellow-aura-accent">
-            <div></div>
-          </CircleButton>
-        </Link>
-      </div>
+      
     </div>
   );
 };
