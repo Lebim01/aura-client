963b9ff37a60d10ba07ea1577b8e73695bbb08b3
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2024-03-11 14:01:13 -0600

    horizontal videos

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta http-equiv="ScreenOrientation" content="autoRotate:disabled" />
+      </Head>
       <body className="hidescroll md:overflow-y-hidden ">
         <Main />
         <NextScript />
