8c13137bf1758319a543f58e29e4d878d5dbc10b
MarcosLeva <marcoslevagomez@gmail.com>
2024-03-11 17:51:14 -0700

    fix bg menu

--- src/components/common/DesktopNavigationButtons.tsx
+++ src/components/common/DesktopNavigationButtons.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { navigationOptions } from "./Footer";
 import Image from "next/image";
-import classNames from "classnames";
 import { usePathname } from "next/navigation";
 import InputSearch from "./InputSearch";
 import { Fragment } from "react";
@@ -11,32 +10,33 @@ const DesktopNavigationButtons = () => {
   const pathname = usePathname();
 
   return (
-    <div className="hidden md:flex flex-col h-fit">
-      <div className="flex-col gap-y-[16px] rounded-[12px] md:min-w-[280px] flex-1">
+    <div className="hidden md:flex flex-col">
+      <div className="flex-col flex gap-y-[16px] rounded-[12px] md:min-w-[280px] flex-1">
         {navigationOptions
           .filter((item) => item.hide === false || item.hide === undefined)
           .map((item, index) => (
-            <Fragment key={index}>
-              <Link
-                href={item.url}
+            <Link
+              href={item.url}
+              className={classNamesCustom("text-[12px] cursor-pointer ")}
+              key={index}
+            >
+              <div
                 className={classNamesCustom(
-                  "text-[12px] hover:scale-105 transition-transform duration-200 ease-in-out cursor-pointer p-[8px] rounded-[6px]",
+                  "flex space-x-[16px] md:space-x-[8px] items-center p-[8px] rounded-[6px]",
                   { "bg-navigation-bg": pathname === item.url }
                 )}
               >
-                <div className="flex space-x-[16px] md:space-x-[8px] items-center">
-                  <Image
-                    src={pathname === item.url ? item.iconactive : item.icon}
-                    alt=""
-                    width={item.size}
-                    height={item.size}
-                  />
-                  <span className="font-[500] leading-[150%] text-white text-[14px]">
-                    {item.title}
-                  </span>
-                </div>
-              </Link>
-            </Fragment>
+                <Image
+                  src={pathname === item.url ? item.iconactive : item.icon}
+                  alt=""
+                  width={item.size}
+                  height={item.size}
+                />
+                <span className="font-[500] leading-[150%] text-white text-[14px]">
+                  {item.title}
+                </span>
+              </div>
+            </Link>
           ))}
       </div>
       <div>
