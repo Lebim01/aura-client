e08a63aeb674af3d190482e1478356b8c4d1dc9a
victor alvarez <victoralvarezsaucedo@gmail.com>
2024-03-14 14:47:05 -0700

    mobile logo

--- src/components/common/NavHeaderMobile.tsx
+++ src/components/common/NavHeaderMobile.tsx
@@ -1,6 +1,5 @@
 import { useSession } from "next-auth/react";
 import CircleButton from "./CircleButton";
-import { LuFilter } from "react-icons/lu";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -10,8 +9,16 @@ const NavHeaderMobile = () => {
 
   return (
     <div className="pb-[16px] flex justify-between items-center">
-      <div className="font-bold">
-        {isLogged ? `Bienvenido ${data?.user?.name}` : "Inicia Sesión"}
+      <div className="md:min-w-[280px]">
+        <Link href="/dashboard">
+          <Image
+            src={"/logo_white.svg"}
+            width={80}
+            height={17}
+            alt=""
+            className="cursor-pointer object-cover object-center hover:scale-110 duration-300 trasition-all"
+          />
+        </Link>
       </div>
       <div className="flex space-x-[8px]">
         <Link href="/profile">
