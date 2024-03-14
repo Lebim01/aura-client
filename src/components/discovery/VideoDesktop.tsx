9c6b1bab2ca593c059e6c6665e223f8c2d08a9fe
victor alvarez <victoralvarezsaucedo@gmail.com>
2024-03-13 19:45:12 -0700

    fix scroll

--- src/components/discovery/VideoDesktop.tsx
+++ src/components/discovery/VideoDesktop.tsx
@@ -4,6 +4,7 @@ import {
   useRef,
   useImperativeHandle,
   useEffect,
+  useState,
 } from "react";
 import canAutoPlay from "can-autoplay";
 import { VideoProps } from "./VideoController";
@@ -32,50 +33,56 @@ const VideoDesktop = forwardRef(
     ref: ForwardedRef<Handler>
   ) => {
     const { muted, setMute } = useVideoMute();
+    const [autoplayMuted, setAutoplayMuted] = useState(true);
     const streamRef = useRef<any>();
 
     useImperativeHandle(ref, () => ({
       play: () => {
-        streamRef.current?.play();
+        playVideo();
       },
       pause: () => {
         streamRef.current?.pause();
       },
     }));
 
-    const togglePlay = () => {
-      setMute(false);
-      if (streamRef.current?.paused) {
-        canAutoPlay.video().then(() => {
-          streamRef.current?.play();
-        });
-      } else {
-        streamRef.current?.pause();
-      }
-    };
+    const playVideo = () => {
+      setTimeout(() => {
+        streamRef.current?.play();
+      }, 300);
 
-    useEffect(() => {
       canAutoPlay.video({ muted: false }).then(({ result }) => {
         if (result) {
-          setMute(false);
+          setAutoplayMuted(false);
+          streamRef.current?.play();
+        } else {
+          canAutoPlay.video({ muted: true }).then(({ result }) => {
+            if (result) {
+              setAutoplayMuted(true);
+              streamRef.current?.play();
+            }
+          });
         }
       });
-    }, []);
+    };
 
     return (
       <div
         className={classNamesCustom(
-          "relative rounded-lg overflow-hidden min-h-[60vh] w-full",
+          "relative rounded-lg overflow-hidden min-h-[60vh] max-h-[70vh] w-full",
           videoOrientation == "vertical" && "md:w-[500px]",
-          videoOrientation == "horizontal" && "py-[10%]"
+          videoOrientation == "horizontal" && "py-[50px]"
         )}
       >
         {/* <VideoHeader /> */}
-        <div className="relative">
-          {muted && (
+        <div className="relative h-full w-full flex justify-center items-center">
+          {autoplayMuted && (
             <button
-              className="absolute top-2 left-2 bg-bg-green-button z-10 p-2 rounded-sm flex items-center space-x-2"
-              onClick={() => setMute(false)}
+              className={classNamesCustom(
+                "absolute left-2 bg-bg-green-button z-10 p-2 rounded-sm flex items-center space-x-2",
+                videoOrientation == "vertical" && "top-2",
+                videoOrientation == "horizontal" && "top-0"
+              )}
+              onClick={() => setAutoplayMuted(false)}
             >
               <MdHearingDisabled /> <span>Reactivar Sonido</span>
             </button>
@@ -85,20 +92,20 @@ const VideoDesktop = forwardRef(
             playsInline
             webkit-playsinline="true"
             controls
-            //src={`https://customer-fuwnvhure6hzod9h.cloudflarestream.com/${videoUrl}`}
+            muted={autoplayMuted}
             ref={streamRef}
             className={classNamesCustom(
-              "cursor-pointer max-h-[80vh]",
-              videoOrientation == "vertical" &&
-                "aspect-tiktok min-w-[300px] h-full w-full",
-              videoOrientation == "horizontal" &&
-                "aspect-video min-w-[500px] w-auto h-auto"
+              "cursor-pointer object-contain",
+              videoOrientation == "vertical" && "aspect-tiktok h-full w-full",
+              videoOrientation == "horizontal" && "aspect-video w-auto h-auto"
             )}
             manifest={`https://customer-fuwnvhure6hzod9h.cloudflarestream.com/${videoUrl}/manifest/video.m3u8`}
             poster={`https://customer-fuwnvhure6hzod9h.cloudflarestream.com/${videoUrl}/thumbnails/thumbnail.jpg?time=1s&height=600`}
           />
           <InfoReview
-            className="bottom-[80px]"
+            className={
+              videoOrientation == "vertical" ? "bottom-[80px]" : "bottom-0"
+            }
             index={videoIndex}
             likes={likes}
             like_me={like_me}
